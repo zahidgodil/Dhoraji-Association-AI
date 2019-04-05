4ce2f58bf1d16543f84bc8f2ea6943fb09e64dab
zahidgodil <48214267+zahidgodil@users.noreply.github.com>
2019-04-05 21:22:57 +0530

    stop watch and prototype dt 5-april-2019

--- app.js
+++ app.js
@@ -1,123 +1,72 @@
-// function Students(name, rollNo, stClass) {
-// this.StudentName = name;
-// this.rollNumber = rollNo;
-// this.studentClass = stClass; 
+
+// STOPWATCH
+// var milliSecond = 0;
+// var second = 0;
+// var minute = 0;
+// var intervalTime;
+// var isTimerStarted = false;
+
+// var currentTime = minute + ' : ' + second + ' : ' + milliSecond;
+// // 0: 0: 0
+// var stopwatch = document.getElementById('stopwatch');
+// stopwatch.innerHTML = currentTime;
+
+// function startTimer(){
+//     if(isTimerStarted !==true){
+//    intervalTime= setInterval(updateTime, 1);
 // }
-
-// var student1 = new Students('zahid', '4564','Mobile & web development' );
-// var student2 = new Students('asad', '4564','Mobile & web development' );
-
-// // console.log(student1);
-// console.log(student1, student2);
-
-
-// function PricePlan(name, price, sitePages, discountedMonths){
-// this.name = name;
-// this.price =  price; 
-// this.sitePages = sitePages;
-// this.discountedMonths = discountedMonths;
-// this.calcAnnual = function(discountedRate){
-//     var currentDate = new Date();
-//     var currentMonth = currentDate.getMonth();
-//     var bestPrice = this.price * 12;
-//     for (var i=0; i < this.discountedMonths.length; i++){
-//         if(this.discountedMonths[i] == currentMonth){
-
-//             bestPrice = bestPrice * discountedRate; 
-//             break;
-//         }
-//     }
-//     return bestPrice;
 // }
+// function updateTime(){
+//     isTimerStarted = true;
 
-// }
-
-// var basicPlan = new PricePlan('basic', 4, '10', [3,6]);
-// var specialPlan = new PricePlan('special', 5, '20', [2,7]);
-// var bestPriceBasic = basicPlan.calcAnnual(0.80);
-// var bestPriceSpecial = specialPlan.calcAnnual(0.80);
-// console.log(bestPriceBasic, bestPriceSpecial)
-
-
-
-
-
-
-
-
-
-// var arr = []
-// function getValues() {
-//     var studentName = document.getElementById("stName").value;
-//     var studentRollNumber = document.getElementById("stRollNo").value;
-//     var studentClass = document.getElementById("stClass").value;
-
-//     var student = {
-//         name: studentName,
-//         rollnumber: studentRollNumber,
-//         stClass: studentClass
-
+//     if(milliSecond < 9){
+//     milliSecond++;
+//     currentTime = minute + ' : ' + second + ' : ' + milliSecond;
+//     stopwatch.innerHTML = currentTime;
 //     }
 
-//     arr.push(student);
-//     console.log(arr);
+//     else if (second < 59){
+//         milliSecond = 0;
+//         second++;
+//         currentTime = minute + ' : ' + second + ' : ' + milliSecond;
+//     stopwatch.innerHTML = currentTime;
 // }
-
-
-
-
-
-
-
-// function Student(name, rollnumber, stClass) {
-//     this.name = name;
-//     this.rollnumber = rollnumber;
-//     this.stClass = stClass;
-
-// }
-// var arr = []
-// function getValues() {
-//     var studentName = document.getElementById("stName").value;
-//     var studentRollNumber = document.getElementById("stRollNo").value;
-//     var studentClass = document.getElementById("stClass").value;
-
-
-//     var studentObject = new Student(studentName, studentRollNumber, studentClass);
-
-//     arr.push(studentObject);
-
-//     console.log(arr);
-// }
-
-// var counter = 0;     -----> wil continue without stop
-
-//  setInterval(function(){
-//     counter++;
-//     console.log(counter);
-
-// },1000);
-
-setTimeout(function(){
-    console.log('welcome to our web');
     
-},5000);
+    
+//     else {
+//         milliSecond = 0;
+//         second = 0;
+//         minute++;
+//         currentTime = minute + ' : ' + second + ' : ' + milliSecond;
+//     stopwatch.innerHTML = currentTime;
+// }
+// }
+
+// function stopTimer(){
+//     isTimerStarted = false;
+//     clearInterval(intervalTime);
+// }
+
+// function clearTimer(){
+//     isTimerStarted = false;
+//     clearInterval(intervalTime);
+//     minute = 0;
+//     second = 0;
+//     milliSecond = 0;
+//     currentTime = minute + ' : ' + second + ' : ' + milliSecond;
+//     stopwatch.innerHTML = currentTime;
+// }
 
 
-// var counter = 0;   -------------> to terminate
+// PROTOTYPE
 
-// var timer = setInterval(function(){
-//     counter++;
-//     console.log(counter);
+function Car(carName,modal, color){
+    this.carName = carName;
+    this.modal = modal;
+    this.color = color;
+}
 
-// },1000);
+Car.prototype.steering = true;
 
-// setTimeout(function(){
-//     console.log('welcome to our web');
-//     clearInterval(timer);
-// },5000);
-
-
-
-
-
-
+var corolla = new Car('Corolla', '2018','Black');
+console.log(corolla)
