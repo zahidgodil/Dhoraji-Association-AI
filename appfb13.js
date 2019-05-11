23269e688f0683236e4ea211e6679cb7ae4d7049
zahidgodil <48214267+zahidgodil@users.noreply.github.com>
2019-05-11 17:32:33 +0500

    11may19

--- appfb13.js
+++ appfb13.js
@@ -1,13 +1,37 @@
+// function createUser() {
+//     var userEmail = document.getElementById('email').value;
+//     var userPassword = document.getElementById('password').value;
+    
+
+
+
+
+
+//     firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword)
+//         .then(() => {
+//             console.log('user created');
+//         })
+//         .catch(function (error) {
+//             // Handle Errors here.
+//             var errorCode = error.code;
+//             var errorMessage = error.message;
+//             // ...
+//         });
+// }
+
+
+
 function createUser() {
     var userEmail = document.getElementById('email').value;
     var userPassword = document.getElementById('password').value;
-
+    var dob = document.getElementById('dob').value;
+    var phone = document.getElementById('phone').value;
 
 
 
 
     firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword)
-        .then(() => {
+        .then((user) => {
             console.log('user created');
         })
         .catch(function (error) {
@@ -17,3 +41,27 @@ function createUser() {
             // ...
         });
 }
+
+
+
+function saveUserDetails(email, phone, dob, uid){
+    console.log(email, phone, dob, uid)
+    firebase.firestore().collection('users').doc(uid).set({
+        userEmail: email,
+        userPhone: phone, 
+        dob: dob,
+        userUid: uid
+    })
+
+        .then(function (docRef){
+            console.log("Document written with ID:",docRef.id);
+        })
+        .catch(function(error){
+            console.log("Error adding document", error);
+        });
+
+
+
+}
+
+
